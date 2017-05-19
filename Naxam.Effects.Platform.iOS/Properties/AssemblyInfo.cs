5d3f6ea97d510a60836630c9dbf0c04ff047f3f9
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-19 13:07:24 +0700

    Publish v1.0.6

--- Naxam.Effects.Platform.iOS/Properties/AssemblyInfo.cs
+++ Naxam.Effects.Platform.iOS/Properties/AssemblyInfo.cs
@@ -10,16 +10,16 @@ using Naxam.Effects.Platform.iOS;
 [assembly: AssemblyDescription("Effects for Xamarin.Forms controls")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Naxam Company Limited")]
-[assembly: AssemblyProduct("Xamarin.Forms Controls")]
-[assembly: AssemblyCopyright("(c) 2017 Naxam")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("X Controls")]
+[assembly: AssemblyCopyright("Copyright (c) 2017 Naxam")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.6")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
--- Naxam.Effects/Properties/AssemblyInfo.cs
+++ Naxam.Effects/Properties/AssemblyInfo.cs
@@ -8,16 +8,16 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Effects for Xamarin.Forms controls")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Naxam Company Limited")]
-[assembly: AssemblyProduct("Xamarin.Forms Controls")]
+[assembly: AssemblyProduct("X Controls")]
 [assembly: AssemblyCopyright("Copyright ©  2017 NAXAM")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("NAXAM")]
 [assembly: AssemblyCulture("")]
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.4")]
+[assembly: AssemblyVersion("1.0.6")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
