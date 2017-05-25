1b5bc1aab2bd65a13d23e12d5421e37e355c9907
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-25 21:36:14 +0700

    Fix issue on ViewStyleEffect on Android

--- Naxam.Effects.Platform.Droid/ViewStyleDroidEffect.cs
+++ Naxam.Effects.Platform.Droid/ViewStyleDroidEffect.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.OS;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using Android.Graphics.Drawables;
-using Android.Content.Res;
 
 namespace Naxam.Effects.Platform.Droid
 {
@@ -22,58 +11,47 @@ namespace Naxam.Effects.Platform.Droid
         {
             UpdateStyle();
         }
+
+        protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
+        {
+            base.OnElementPropertyChanged(args);
+
+            if (args.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
+            {
+                UpdateStyle();
+            }
+        }
+
         void UpdateStyle()
         {
-            try
+            var view = Control ?? Container;
+            var bgColor = (Element as VisualElement)?.BackgroundColor ?? Color.Transparent;
+
+            PaintDrawable paint = new PaintDrawable(bgColor.ToAndroid());
+            GradientDrawable gradient = new GradientDrawable();
+            paint.SetCornerRadius(Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.CornerRadiusProperty)));
+            gradient.SetCornerRadius(Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.CornerRadiusProperty)));
+            gradient.SetColor(Color.Transparent.ToAndroid());
+            gradient.SetOrientation(GradientDrawable.Orientation.LeftRight);
+            gradient.SetShape(ShapeType.Rectangle);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
             {
-                var view = Control ?? Container;
-
-                PaintDrawable paint = new PaintDrawable(Color.Transparent.ToAndroid());
-                GradientDrawable gradient = new GradientDrawable();
-                paint.SetCornerRadius(Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.CornerRadiusProperty)));
-                gradient.SetCornerRadius(Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.CornerRadiusProperty)));
-                gradient.SetColor(Color.Transparent.ToAndroid());
-                gradient.SetOrientation(GradientDrawable.Orientation.LeftRight);
-                gradient.SetShape(ShapeType.Rectangle);
-
-                if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
-                {
-                    view.Elevation = Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.ShadowOffsetYProperty));
-                    view.TranslationZ = Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.ShadowOffsetYProperty));
-                }
-                
-                gradient.SetStroke((int)Forms.Context.ToPixels(ViewEffect.GetBorderWidth(Element)), ((Color)Element.GetValue(ViewEffect.BorderColorProperty)).ToAndroid());
-                LayerDrawable layer = new LayerDrawable(
-                    new Drawable[]
-                    {
-                        paint,gradient
-                    });
-                view.SetBackground(layer);
+                view.Elevation = Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.ShadowOffsetYProperty));
+                view.TranslationZ = Forms.Context.ToPixels((double)Element.GetValue(ViewEffect.ShadowOffsetYProperty));
             }
-            catch { }
+
+            gradient.SetStroke((int)Forms.Context.ToPixels(ViewEffect.GetBorderWidth(Element)), ((Color)Element.GetValue(ViewEffect.BorderColorProperty)).ToAndroid());
+            LayerDrawable layer = new LayerDrawable(
+                new Drawable[]
+                {
+                        paint,gradient
+                });
+            view.SetBackground(layer);
         }
 
         protected override void OnDetached()
         {
-            // throw new NotImplementedException();
         }
-        //protected override void OnElementPropertyChanged(System.ComponentModel.PropertyChangedEventArgs args)
-        //{
-        //    base.OnElementPropertyChanged(args);
-        //    var view = Control ?? Container;
-        //    if (args.PropertyName == "Height")
-        //    {
-
-        //        var newHeight = (int)Forms.Context.ToPixels((double)Element.GetValue(VisualElement.HeightProperty));
-        //        view.Layout(0, 0, view.Width, newHeight);
-        //        UpdateStyle();
-        //    }
-        //    else if (args.PropertyName == "Width")
-        //    {
-        //        var newWeight = (int)Forms.Context.ToPixels((double)Element.GetValue(VisualElement.WidthProperty));
-        //        view.Layout(0, 0, newWeight, view.Height);
-        //        UpdateStyle();
-        //    }
-        //}
     }
 }
